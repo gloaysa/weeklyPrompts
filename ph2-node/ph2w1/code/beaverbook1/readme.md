38856c2746841eb8dcccfa4975f98041fdd3049c
Evan Cole <master@Evans-MacBook-Pro.local>
2017-05-04 01:48:36 +0200

    done?

--- ph2-node/ph2w1/code/beaverbook1/readme.md
+++
@@ -1,1 +0,0 @@
-replace array with mongoose
--- ph2-node/ph2w3/code/classExample/models/session.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
-
-var sessionSchema = new Schema({
-    userID:    {type: String, required: true}
-})
-
-mongoose.model("sessionCollection", sessionSchema)
